0339e5b79236f12b6113c35101f1efa6f9ffef68
Michael Usachenko <senseikawasaki@gmail.com>
2018-05-18 16:12:59 -0500

    added dependency version scrape

--- dockerize.py
+++ dockerize.py
@@ -1,27 +1,21 @@
 import sys 
 import glob
-
-import subprocess
-from multiprocessing import Pool
+from multiprocessing.dummy import Pool
 from multiprocessing import cpu_count
 from constants import stdlib_py_all
 
-### Extract all filepaths of target directory
-### Get set of all imports 
+import requests 
+import json
 
 def get_local_modules(all_paths):
 	local = []
 	for path in all_paths:
-		mod = path.split('/')
-		mod = mod[len(mod)-1]
+		mod = path.split('/')[-1]
 		local.append(mod.split('.')[0])
 	return set(local)
 
+### Extract all names of non-local imports 
 def get_all_imports(dir, ext, version):
-	#pwd = dir.split('\n')[0] #remove newline
-	#pwd = str(pwd) + '/*' + '.py' # TODO: support more extensions
-	#filepaths = glob.glob(pwd)
-
 	filepaths = glob.glob(dir + '/**/*.{}'.format(ext), recursive=True)
 	local_modules = get_local_modules(filepaths)
 
@@ -38,24 +32,36 @@ def get_all_imports(dir, ext, version):
 	all_imports = set([elem.strip('.') for elem in all_imports])
 	return list(all_imports - stdlib_py_all[version] - local_modules)
 
-if __name__ == '__main__':
-	print(get_all_imports(sys.argv[1], sys.argv[2], sys.argv[3]))
-	
+### Get Release Info for all non-local imports 
+
+def get_release_info_imports(all_imports):
+	def get_wrapper(import_name):
+		url = 'https://pypi.python.org/pypi/{}/json'.format(import_name)
+		return (import_name, requests.get(url).json())
+
+	pool = Pool(cpu_count())
+	all_release_info = pool.map(get_wrapper, all_imports)
+	pool.close()
+	pool.join()
+
+	latest_releases = []
+	for r_info in all_release_info:
+		import_name, info_dict = r_info
+		latest_release = list(info_dict['releases'].keys())[-1]
+		latest_releases.append((import_name, latest_release))
+
+	return latest_releases
+
 ### Create Dockerfile.txt ###
 #dockerfile = open('Dockerfile.txt', 'w')
-
-"""
-pool = Pool(cpu_count())
-### Analyze Dependencies using pipreqs 
-pool.map(subprocess.run, filepaths)
-pool.close()
-pool.join()
-"""
 
 ### Write to Dockerfile.txt
 
 ### Create Image 
 
 ### Test Run 
-
+if __name__ == '__main__':
+	imports = get_all_imports(sys.argv[1], sys.argv[2], sys.argv[3])
+	imports_release_info = get_release_info_imports(imports)
+	print(imports_release_info)
 
